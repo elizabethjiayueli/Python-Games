43b0fe8054303e0fa501eb4748a68326967152c3
elizabethjiayueli <moxueqin@hotmail.com>
2025-09-09 00:00:56 +0000

    Day 47: Adding Explosion on Collision

---
+++ lessons/00_Getting_Started/hotel_management.py
@@ -0,0 +1,61 @@
+"""
+Hotel Management System
+1. Functions needed:
+    1a. Be able to check in or check out
+    1b. recieve price
+    1c. choose room
+    1d. how many guests
+5. Should use lists, tuples, dictionaries
+6. All functionality in functions
+"""
+from tkinter import messagebox, simpledialog,Tk
+window = Tk()
+window.withdraw()
+db = {}
+
+def in_or_out(db):
+    
+    rooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    while True:
+        print(db)
+        ask = simpledialog.askstring("HOTEL MANAGEMENT", "What is your name?")
+        check = simpledialog.askstring("HOTEL MANAGEMENT", "Would you like to check in or check out?")
+        if check == "check in" or check == "in":
+            selected_rooms = []
+            peeps_num = simpledialog.askinteger("HOTEL MANAGEMENT", "How many people are you traveling with?")
+            if int(peeps_num) <= 6:
+                selected_rooms.append(room_func(rooms))
+                days = simpledialog.askinteger("HOTEL MANAGEMENT", "How many days are you going to stay?")
+                price = 200*days + peeps_num
+            elif int(peeps_num) >= 7:
+                num_rooms = (simpledialog.askinteger("HOTEL MANAGEMENT", "Please choose 2 or more rooms. Enter the number of rooms you would like."))
+                for i in range(num_rooms):
+                    selected_rooms.append(room_func(rooms))
+                days = simpledialog.askinteger("HOTEL MANAGEMENT", "How many days are you going to stay?")
+                price = 200*days + peeps_num * num_rooms
+            db[ask] = selected_rooms
+            messagebox.showinfo("HOTEL MANAGEMENT",f"Your price is $ {price}")
+        elif check == "check out" or check == "out":
+            if ask in db:
+                db.pop(ask) 
+                messagebox.showinfo("HOTEL MANAGEMENT", "I hope you enjoyed your stay!")
+            else:
+                messagebox.showerror("error")
+        else:
+            messagebox.showwarning("invalid response", "please try again")
+        messagebox.showinfo("HOTEL MANAGEMENT", db)
+        continue_ = simpledialog.askstring("HOTEL MANAGEMENT", "Would you like to continue checking in?")
+        if continue_ == "yes":
+            pass
+        elif continue_ == "no":
+            break
+
+def room_func(rooms):
+    room = simpledialog.askinteger("Which room would you like?", rooms)
+    if room in rooms:
+        rooms.remove(room)
+    else:
+        messagebox.showerror("HOTEL MANAGEMENT", "This room is not available. Please try again.")
+        room = simpledialog.askinteger("Which room would you like?", rooms)
+    return room
+in_or_out(db)
--- lessons/07_Projects/02_Invaders/main.py
+++ lessons/07_Projects/02_Invaders/main.py
@@ -21,6 +21,7 @@ class Settings:
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption('SPACE INVADERS')  
     FPS = 30
+    red = (255, 8, 0)
 
     projectile_speed = 10
     shoot_delay = 250  # 250 milliseconds between shots, or 4 shots per second   
@@ -28,6 +29,7 @@ class Settings:
     WIDTH, HEIGHT = 600, 300
     PLAYER_SIZE = 25
     position = (100, 1000)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, game):
         super().__init__()
@@ -38,6 +40,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = Settings.position)
         self.rect.x = 50
         self.rect.y = Settings.HEIGHT - Settings.PLAYER_SIZE -7
+        print(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
         self.rect = self.image.get_rect()
         self.last_shot = pygame.time.get_ticks()
         self.shoot_delay = Settings.shoot_delay  
@@ -91,13 +94,52 @@ class Player(pygame.sprite.Sprite):
 class Alien(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-
-class Bomb(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.original_image = pygame.image.load(assets/'bomb.png')
+        self.original_image = pygame.image.load(assets/'alien.png')
         self.image = pygame.transform.scale(self.original_image, (Settings.width, Settings.height))
         self.rect = self.image.get_rect()
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, position, velocity=Settings.projectile_speed/2):
+        super().__init__()
+        self.original_image = pygame.image.load(assets/'bomb.gif')
+        self.image = pygame.transform.scale(self.original_image, (10, 15))
+        self.rect = self.image.get_rect()
+        self.rect.x = position[0]
+        self.rect.y = position[1]
+        self.velocity = pygame.Vector2(0, 1)  * velocity
+        self.explosion = pygame.image.load(assets / "explosion1.gif")
+    def explode(self):
+        """Replace the image with an explosition image."""
+        
+        # Load the explosion image
+        self.image = self.explosion
+        self.image = pygame.transform.scale(self.image, (10, 10))
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.x_vel = 0
+        self.y_vel = 0
+        print("exploded")
+    def update(self):
+        self.rect.center += self.velocity
+        if self.rect.y >= Settings.screen_height:
+            self.kill()
+            print("bomb killed")
+        
+        super().update()
+def add_obstacle(obstacles):
+    # random.random() returns a random float between 0 and 1, so a value
+    # of 0.25 means that there is a 25% chance of adding an obstacle. Since
+    # add_obstacle() is called every 100ms, this means that on average, an
+    # obstacle will be added every 400ms.
+    # The combination of the randomness and the time allows for random
+    # obstacles, but not too close together. 
+    key = pygame.key.get_pressed()
+    if key[pygame.K_b]:
+        bomb = Bomb(
+            position=(100, 100)
+        )
+        print("bomb released")
+        game.bombs.add(bomb)
+        return 1
+    return 0
 
     
 
@@ -110,31 +152,50 @@ class Projectile(pygame.sprite.Sprite):
         self.rect.x = position[0]
         self.rect.y = position[1]
         self.velocity = pygame.Vector2(0, -1)  * velocity
+        
     def update(self):
         self.rect.center += self.velocity
+        if self.rect.center[1] <= 0 or self.rect.y >= Settings.screen_height:
+            self.kill()
+            print("projectile killed")
         
         super().update()
     
 class Game:
     def __init__(self):
+        #sprite groups
         self.player = Player(self)
         self.player_group = pygame.sprite.GroupSingle(self.player)
         self.projectiles = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.bombs = pygame.sprite.Group()
+        
+
+        #health
+        self.health = 3
+        self.health_bars = None
+
+        #background
         self.full_background = self.make_tiled_bg(Settings.screen, assets/'space.png')
-    
+        pygame.draw.rect(Settings.screen, Settings.red, self.player.rect)
+        #pygame.draw.rect(Settings.screen, Settings.red, self.bombs.rect)
     def make_tiled_bg(self, screen, background):
         # Scale background to match the screen height
-        
         bg_tile = pygame.image.load(background).convert()
-        
         background_height = Settings.screen.get_height()
         bg_tile = pygame.transform.scale(bg_tile, (bg_tile.get_width(), Settings.screen.get_height()))
         # Get the dimensions of the background after scaling
         background_width = bg_tile.get_width()
         return bg_tile
-    
-
-     
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            collider = pygame.sprite.spritecollide(self.player, self.bombs, False)
+            if collider:
+                collider[0].explode()
+                print("hit by bomb")
+        
 game = Game()
 
 running = False
@@ -146,6 +207,17 @@ while running:
     game.player_group.draw(Settings.screen)
     game.projectiles.update()
     game.projectiles.draw(Settings.screen)
+    game.enemies.update()
+    game.enemies.draw(Settings.screen)
+    add_obstacle(game.bombs)
+    game.bombs.update()
+    game.bombs.draw(Settings.screen)
+    game.handle_events()
+    health_1 = pygame.draw.circle(Settings.screen, Settings.red, (129, 410), 5)
+    health_2 = pygame.draw.circle(Settings.screen, Settings.red, (144, 410), 5)
+    health_3 = pygame.draw.circle(Settings.screen, Settings.red, (159, 410), 5)
+    game.health_bars = [health_1, health_2, health_3]
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
